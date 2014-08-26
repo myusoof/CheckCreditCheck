cbc775d611b4be7c719f06ae61c8c115b8f976fd
mohmedy <mohmedyusoof.as@cognizant.com>
2014-08-26 11:28:16 +0100

    Exception handling and getting the tariff details

--- src/test/groovy/uk/CreditCheckTest.groovy
+++ src/test/groovy/uk/CreditCheckTest.groovy
@@ -26,6 +26,7 @@ import static javax.ws.rs.client.Entity.json
  * To change this template use File | Settings | File Templates.
  */
 class CreditCheckTest {
+    StringBuilder stringBuilderHeader = new StringBuilder()
     StringBuilder stringBuilder = new StringBuilder()
     protected JsonFactory jsonFactory = new MappingJsonFactory()
     ObjectMapper objectMapper = new ObjectMapper()
@@ -47,39 +48,69 @@ class CreditCheckTest {
         consumerMap.add("X-IdType", "IdentityUID")
         consumerMap.add("X-Channel", "ConsumerUpgrade")
         consumerMap.add("X-UserId", "ID-000100")
-        stringBuilder.append("InternalOrderId, journeyType, createdTime, status, devicedescription, deviceSKU,creditCheckStatusCondition, creditCheckStatus, contactNumber, creditVetNumber, creditScore, lastCreditCheckDate, referralReason")
-        stringBuilder.append("\n")
+        stringBuilderHeader.append("InternalOrderId, msisdn, portalId, isPreOrder, journeyType, createdTime, status, devicedescription, deviceSKU,tariffDescription, tariffPID, creditCheckStatusCondition, creditCheckStatus, contactNumber, creditVetNumber, creditScore, lastCreditCheckDate, referralReason")
+        stringBuilderHeader.append("\n")
+        File file = new File("./orderDetails${new SimpleDateFormat("yyyyMMdd").format(new Date())}.csv")
+        FileWriter fileWriter = new FileWriter(file.name,true);
+        fileWriter.write(stringBuilderHeader.toString())
         Object response = getRequest(target,consumerMap, "https://prodcat.o2.co.uk/orderService/reports/orders", true)
-
+        String internalOrderId
+        String msisdn
+        String portalId
+        String isPreOrder
+        String journeyType
+        String createdTime
+        String status
+        String deviceDescription
+        String deviceSKU
+        String tariffDescription
+        String tariffPID
+        String creditCheckStatusCondition
+        String creditCheckStatus
+        String contactNumber
+        String creditVetNumber
+        String creditScore
+        String lastCreditCheckDate
+        String referralReason
 
         response.each {
-            String orderUrl = it.toString().replaceFirst("http.*8080", "https://prodcat.o2.co.uk")
-            println "Currently processing ${orderUrl}"
-            Object orderDetail = getRequest(target, consumerMap, orderUrl, false)
-            String internalOrderId = orderDetail?.id
-            String journeyType = orderDetail?.journeyType
-            String createdTime = orderDetail?.createdTime
-            String status = orderDetail?.status
-            String deviceDescription = orderDetail?.lineItems?.find{it.type == 'device'}?.description
-            String deviceSKU = orderDetail?.lineItems?.find{it.type == 'device'}?.skuOrPid
-            String creditCheckStatusCondition = orderDetail?.requirements?.creditCheckDetails?.met?:null
-            String creditCheckStatus = orderDetail.requirements?.creditCheckDetails?.creditCheckStatus?:null
-            String contactNumber= orderDetail?.requirements?.creditCheckDetails?.contactNumber?:null
-            String creditVetNumber= orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.creditVetNumber?:null
-            String creditScore    = orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.creditScore?:null
-            String lastCreditCheckDate= orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.lastCreditCheckDate?:null
-            String referralReason = orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.referralReason?:null
+            try {
+                String orderUrl = it.toString().replaceFirst("http.*8080", "https://prodcat.o2.co.uk")
+                println "Currently processing ${orderUrl}"
+                Object orderDetail = getRequest(target, consumerMap, orderUrl, false)
+                internalOrderId = orderDetail?.id
+                msisdn = orderDetail?.msisdn?:null
+                portalId = orderDetail?.customerInfo?.portalId
+                isPreOrder = orderDetail?.lineItems?.find { it.type == 'device' }?.stock == "PreOrder"
+                journeyType = orderDetail?.journeyType
+                createdTime = orderDetail?.createdTime
+                status = orderDetail?.status
+                deviceDescription = orderDetail?.lineItems?.find { it.type == 'device' }?.description
+                deviceSKU = orderDetail?.lineItems?.find { it.type == 'device' }?.skuOrPid
+                tariffDescription = orderDetail?.lineItems?.find { it.type == "plan" }?.description
+                tariffPID = orderDetail?.lineItems?.find { it.type == "plan" }?.skuOrPid
+                creditCheckStatusCondition = orderDetail?.requirements?.creditCheckDetails?.met ?: null
+                creditCheckStatus = orderDetail.requirements?.creditCheckDetails?.creditCheckStatus ?: null
+                contactNumber = orderDetail?.requirements?.creditCheckDetails?.contactNumber ?: null
+                creditVetNumber = orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.creditVetNumber ?: null
+                creditScore = orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.creditScore ?: null
+                lastCreditCheckDate = orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.lastCreditCheckDate ?: null
+                referralReason = orderDetail?.requirements?.creditCheckDetails?.creditVetDetails?.referralReason ?: null
+            }catch(Exception e){
+               println "Not able to process ${internalOrderId}"
+            }finally{
+                stringBuilder.append("${internalOrderId}, ${msisdn}, ${portalId}, ${isPreOrder}, ${journeyType}, ${createdTime}, ${status}, ${deviceDescription}, ${deviceSKU}, ${tariffDescription}, ${tariffPID}, ${creditCheckStatusCondition}, ${creditCheckStatus}, ${contactNumber}, ${creditVetNumber}, ${creditScore}, ${lastCreditCheckDate}, ${referralReason}")
+                stringBuilder.append("\n")
+                fileWriter.write(stringBuilder.toString())
+                stringBuilder.setLength(0)
+            }
 
-            stringBuilder.append("${internalOrderId}, ${journeyType}, ${createdTime}, ${status}, ${deviceDescription}, ${deviceSKU}, ${creditCheckStatusCondition}, ${creditCheckStatus}, ${contactNumber}, ${creditVetNumber}, ${creditScore}, ${lastCreditCheckDate}, ${referralReason}")
-            stringBuilder.append("\n")
         }
-        File file = new File("./orderDetails${new SimpleDateFormat("yyyyMMdd").format(new Date())}.csv")
-        file.setText(stringBuilder.toString())
+
+
     }
 
-    Object getRequest(WebTarget target,MultivaluedHashMap map, String path, boolean queryParam){
-
-
+    Object getRequest(WebTarget target,MultivaluedHashMap map, String path, boolean queryParam) throws Exception{
         target = target.path(path)
 
         if(queryParam){
